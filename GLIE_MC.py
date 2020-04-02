3f957c30effcf2d5b5c04964e9dc5153ff9417da
Yeso <vitoryeso@outlook.com>
2020-04-02 13:12:19 -0300

    Add files via upload

--- GLIE_MC.py
+++ GLIE_MC.py
@@ -2,10 +2,12 @@ import numpy as np
 import gym
 import gym_maze
 import matplotlib.pyplot as plt
+from time import sleep
+
 
 from collections import defaultdict
 
-class MC_agent(self):
+class MC_agent:
     def __init__(self, env, gamma, alpha, start_eps, eps_step):
         self.env = env
         self.gamma = gamma
@@ -13,21 +15,26 @@ class MC_agent(self):
         self.eps = start_eps
         self.eps_step = eps_step
 
+        self.maze_act = ['N', 'S', 'W', 'E']
         self.nA = env.action_space.n
         self.Q = defaultdict(lambda: np.zeros(self.nA))
     
     def e_greedy(self, s, eps):
         policy = (np.ones(self.nA)*eps)/self.nA
         a_max = np.argmax(self.Q[s])
-        policy[a_max] = 1 - eps - eps/self.nA
+        policy[a_max] = 1 - eps + eps/self.nA
         return policy
     
-    def play_episode(self)
+    def play_episode(self, show=False, delay=1):
         episode = []
-        s = env.reset()
+        state = tuple(self.env.reset())
         while(1):
-            act = np.random.choice(4, 1, self.e_greedy(s, self.eps))[0]
-            next_state, reward, done, _ = selv.env.step(act)
+            if show:
+                sleep(delay)
+                self.env.render()
+            act = np.random.choice(4, 1, p=self.e_greedy(state, self.eps))[0]
+            next_state, reward, done, _ = self.env.step(self.maze_act[act])
+            next_state = tuple(next_state)
             episode.append((state, act, reward))
             state = next_state
             if done:
@@ -36,13 +43,13 @@ class MC_agent(self):
 
     def sum_rewards(self, rewards, i):
         soma = 0
-        for j, n in enumerate(range(i, len(rewards))):
-            soma += rewards[j] * (gamma**n)
+        for n, j in enumerate(range(i, len(rewards))):
+            soma += rewards[j] * (self.gamma**n)
         return soma
 
     def update_knowledge(self, episode, mode='first'):
         states, actions, rewards = zip(*episode)
-        SA_pair = zip((states, actions))
+        SA_pair = tuple(zip(states, actions))
         if mode == 'first':
             visited = []
             for i in range(len(SA_pair)):
--- play.py
+++ play.py
@@ -1,1 +1,25 @@
-da
+import gym
+import gym_maze
+import numpy as np
+import time
+
+
+from GLIE_MC import MC_agent
+
+env = gym.make('maze-random-30x30-plus-v0')
+agent = MC_agent(env, 1.0, 0.02, 1, 0.2)
+
+r = []
+n_episodes = 100
+for n in range(1, n_episodes + 1):
+    agent.eps = 0.9 if n < 90 else 0.1
+    episode = agent.play_episode()
+    r.append(episode[-1][-1])
+    if n%10 == 0:
+        template = 'Episode: {}/{}, Rewards: {}, Epsilon: {}'
+        print(template.format(n, n_episodes, np.mean(r), agent.eps))
+        r = []
+    agent.update_knowledge(episode)
+    
+agent.play_episode(show=True, delay=0.2)
+
