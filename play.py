a1dfca44f397aa03bb1c4322d7b610503e21e6a6
Yeso <vitoryeso@outlook.com>
2020-04-02 13:41:47 -0300

    Add files via upload

--- play.py
+++ play.py
@@ -10,16 +10,17 @@ env = gym.make('maze-random-30x30-plus-v0')
 agent = MC_agent(env, 1.0, 0.02, 1, 0.2)
 
 r = []
-n_episodes = 100
+n_episodes = 1000
 for n in range(1, n_episodes + 1):
-    agent.eps = 0.9 if n < 90 else 0.1
+    agent.eps = 0.8 if n < 900 else 0.1
     episode = agent.play_episode()
-    r.append(episode[-1][-1])
+    _, _, rewards = zip(*episode)
+    r.append(np.mean(rewards))
     if n%10 == 0:
         template = 'Episode: {}/{}, Rewards: {}, Epsilon: {}'
         print(template.format(n, n_episodes, np.mean(r), agent.eps))
         r = []
     agent.update_knowledge(episode)
-    
-agent.play_episode(show=True, delay=0.2)
+for n in range(10):
+    agent.play_episode(show=True, delay=0.06)
 
