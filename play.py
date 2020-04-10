f5bafdd9471b4a0d84434495d74ecfb373c389a5
Yeso <vitoryeso@outlook.com>
2020-04-10 12:38:02 -0300

    Update play.py

--- play.py
+++ play.py
@@ -8,7 +8,7 @@ from GLIE_MC import MC_agent
 
 env = gym.make('maze-random-30x30-plus-v0')
 agent = MC_agent(env, 0.99, 0.02, 0.9, 0.999)
-
+min_eps = 0.1
 r = []
 n_episodes = 1000
 for n in range(1, n_episodes + 1):
@@ -20,7 +20,7 @@ for n in range(1, n_episodes + 1):
         print(template.format(n, n_episodes, np.mean(r), agent.eps))
         r = []
     agent.update_knowledge(episode)
-    agent.eps *= agent.eps_step 
+    agent.eps = max(min_eps, agent.eps_step * agent.eps) 
 for n in range(10):
     agent.play_episode(show=True, delay=0.06)
 
